ba412fd3a9673587ead7151dd80abd7c353328b1
Kartik Gupta <kartikgupta7267@gmail.com>
2023-09-03 21:10:50 +0530

    test 3

--- frontend_v2/vite.config.ts
+++ frontend_v2/vite.config.ts
@@ -10,5 +10,13 @@ export default defineConfig(() => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
+    server: {
+      proxy: {
+        "/api": {
+          target: "https://demo.qdrant.tech",
+          changeOrigin: true,
+        },
+      },
+    }
   };
 });
