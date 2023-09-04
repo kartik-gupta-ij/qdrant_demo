77e8d52af0b3a139e899ba0c0e30fa75fc2ce52b
Kartik Gupta <kartikgupta7267@gmail.com>
2023-09-04 11:49:10 +0530

    font changes

--- frontend_v2/src/components/MainSection/style.tsx
+++ frontend_v2/src/components/MainSection/style.tsx
@@ -1,83 +1,91 @@
 import { createStyles, rem } from "@mantine/core";
 
 export const useStyles = createStyles((theme) => ({
-    wrapper: {
-      position: "relative",
-      paddingTop: rem(120),
-      paddingBottom: rem(30),
-  
-      [theme.fn.smallerThan("sm")]: {
-        paddingTop: rem(80),
-        paddingBottom: rem(60),
-      },
-    },
-  
-    inner: {
-      position: "relative",
-      zIndex: 1,
-    },
-  
-    title: {
-      textAlign: "center",
-      marginBottom: theme.spacing.xs,
-      // fontSize: "80px",
-      // lineHeight:"96px"
-    },
-  
-    highlight: {
-      color: theme.colors[theme.primaryColor][2],
-    },
-  
-    description: {
-      textAlign: "center",
-      // fontSize: "24px",
-      // lineHeight:"32px",
-      [theme.fn.smallerThan("xs")]: {
-        fontSize: theme.fontSizes.md,
-      },
-    },
-    controls:{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: theme.spacing.md,
-      flexDirection: "column",
-      width: "100%",
-    },
-    inputArea: {
-      width: "100%",
-      [theme.fn.smallerThan("xs")]: {
-        height: rem(28),
-        fontSize: theme.fontSizes.md,
-      },
-      "& .mantine-TextInput-rightSection	":{
-        width: "auto",
-      }
-    },
-    inputRightSection:{
-      display: "flex",
-      alignItems: "center",
-      gap: theme.spacing.xs,
-      paddingLeft: 10,
-      paddingRight: 10,
-      marginRight: 5,
-      zIndex: 1,
-      backgroundColor: "white",
-    },
-    control: {
-      marginTop: theme.spacing.md,
-      [theme.fn.smallerThan("xs")]: {
-        height: rem(28),
-        fontSize: theme.fontSizes.md,
-      },
-      transition: "transform .3s ease-in-out",
-      "&:hover ": {
-        transform: "scale(1.1)",
-      },
-    },
-    viewResult:{
-      paddingTop: theme.spacing.md,
-    }
-  }));
+  wrapper: {
+    position: "relative",
+    paddingTop: rem(120),
+    paddingBottom: rem(80),
 
- 
+    [theme.fn.smallerThan("sm")]: {
+      paddingTop: rem(80),
+      paddingBottom: rem(60),
+    },
+  },
+
+  inner: {
+    position: "relative",
+    zIndex: 1,
+  },
+  title: {
+    textAlign: "center",
+    fontWeight: 800,
+    fontSize: rem(40),
+    letterSpacing: -1,
+    marginBottom: theme.spacing.xs,
+    [theme.fn.smallerThan("xs")]: {
+      fontSize: rem(28),
+      textAlign: "left",
+    },
+  },
+
+  highlight: {
+    color:
+      theme.colors[theme.primaryColor][2],
+  },
+
+  description: {
+    textAlign: "center",
+
+    [theme.fn.smallerThan("xs")]: {
+      textAlign: "left",
+      fontSize: theme.fontSizes.md,
+    },
+  },
+
+  controls: {
+    marginTop: theme.spacing.lg,
+    display: "flex",
+    justifyContent: "center",
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "column",
+    },
+  },
+
+  control: {
+    "&:not(:first-of-type)": {
+      marginLeft: theme.spacing.md,
+    },
+
+    [theme.fn.smallerThan("xs")]: {
+      height: rem(42),
+      fontSize: theme.fontSizes.md,
+      "&:not(:first-of-type)": {
+        marginTop: theme.spacing.md,
+        marginLeft: 0,
+      },
+    },
+  },
+  inputRightSection:{
+    display: "flex",
+    alignItems: "center",
+    gap: theme.spacing.xs,
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginRight: 5,
+    zIndex: 1,
+    backgroundColor: "white",
+  },
+  inputArea: {
+    width: "100%",
+    [theme.fn.smallerThan("xs")]: {
+      height: rem(28),
+      fontSize: theme.fontSizes.md,
+    },
+    "& .mantine-TextInput-rightSection	":{
+      width: "auto",
+    }
+  },
+  viewResult:{
+    paddingTop: theme.spacing.md,
+  }
+}));
